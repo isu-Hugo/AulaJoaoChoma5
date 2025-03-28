79d91625eda1bb310a402ab71a92acadd5861e51
isu-Hugo <ehugo209@gmail.com>
2025-03-27 22:25:14 -0300

    cmt

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function somar(){
+    input1 = parseInt(document.getElementById('numero1').value);
+    input2 = parseInt(document.getElementById('numero2').value);
+        
+    document.getElementById('resultado').innerHTML = (input1+input2)
+}
